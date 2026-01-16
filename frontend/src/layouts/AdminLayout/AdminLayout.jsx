4876a3f9051faa951795dc1f43e3ee949ba7123f
Tan Nickole <nickoletan12@gmail.com>
2026-01-16 13:13:25 +0800

    i18n caller

--- frontend/src/layouts/AdminLayout/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout/AdminLayout.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 const AdminLayout = () => {
-    
+    const { t } = useTranslation();
+
     return <>
+        {t('welcome')}
         <Outlet/>
     </>;
 };
